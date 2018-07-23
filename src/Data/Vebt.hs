4c0da83935b845b4d04302697b305e034946cf68
Dmitry Kovanikov <kovanikov@gmail.com>
2018-07-23 19:17:31 +0800

    [#3] Implement insert function

--- src/Data/Vebt.hs
+++ src/Data/Vebt.hs
@@ -7,6 +7,7 @@ module Data.Vebt
 
 import Prelude hiding (lookup)
 
+import Data.Bits (shiftL, shiftR, (.&.))
 import Data.IntMap (IntMap)
 import Data.Word (Word64)
 
@@ -25,7 +26,7 @@ data VEBT v
 
     -- | Minimum in whole tree and children. On each depth size of machine word
     -- is diveded by 2 but this is not yet expressed in types.
-    | Branch Word64 (IntMap (VEBT v))
+    | Branch Word64 !v (IntMap (VEBT v))
     deriving (Eq, Show)
 
 empty :: VEBT v
@@ -42,9 +43,34 @@ lookup k = go 6  -- 6 as in 2^6 = 64
 
 {- | Lookup for element.
 -}
-insert :: Word64 -> v -> VEBT v -> VEBT v
-insert k v = go 6  -- 6 as in 2^6 = 64
+insert :: forall v . Word64 -> v -> VEBT v -> VEBT v
+insert = go 64
   where
-    -- takes k -- current size of machine word
-    go :: Int -> VEBT v -> VEBT v
-    go = error "Not implemented!"
+    -- takes w -- current size of machine word
+    go :: Int -> Word64 -> v -> VEBT v -> VEBT v
+    go _ k v Empty = Leaf k v
+    go w k v (Leaf tMin tVal) = case k `compare` tMin of
+        EQ -> Leaf tMin v
+        LT -> let (hi, lo) = split w tMin
+              in Branch k v $ IM.singleton (fromIntegral hi) $ Leaf lo tVal
+        GT -> let (hi, lo) = split w k
+              in Branch tMin tVal $ IM.singleton (fromIntegral hi) $ Leaf lo v
+    go w k v (Branch tMin tVal children) = case k `compare` tMin of
+        EQ -> Branch tMin v children
+        LT -> let (hi, lo) = split w tMin
+              in Branch k v $ IM.alter (Just . addNode lo tVal w) (fromIntegral hi) children
+        GT -> let (hi, lo) = split w k
+              in Branch tMin tVal $ IM.alter (Just . addNode lo v w) (fromIntegral hi) children
+
+    addNode :: Word64 -> v -> Int -> Maybe (VEBT v) -> VEBT v
+    addNode k v _ Nothing  = Leaf k v
+    addNode k v w (Just t) = go (w `div` 2) k v t
+
+----------------------------------------------------------------------------
+-- utility functions
+----------------------------------------------------------------------------
+
+-- | Split number into (hi, lo).
+split :: Int -> Word64 -> (Word64, Word64)
+split w x = let half = w `shiftR` 1  -- fast division by 2
+            in (x `shiftR` half, x .&. ((1 `shiftL` half) - 1))
